f32775ef3eb086d9321fd26775f07a82eb31128e
enzo <dartmolecules@gmail.com>
2022-02-09 00:52:22 -0800

    initial commit

---
+++ android/app/src/main/kotlin/app/dartdev/musicplayer/simple_music_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.dartdev.musicplayer.simple_music_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:simple_music_player/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
