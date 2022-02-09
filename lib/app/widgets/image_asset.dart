95975123bd86ebbfb182456046347d8f8ab1b5a1
enzo <dartmolecules@gmail.com>
2022-02-09 02:00:38 -0800

    impl image asset widget & add some images

---
+++ lib/app/widgets/image_asset.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ImageAsset extends StatelessWidget {
+  final String? image;
+  final double? width;
+  final double? height;
+  final BoxFit? fit;
+
+  const ImageAsset(
+      {Key? key,
+        required this.image,
+      this.width = 24,
+      this.height = 24,
+      this.fit})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/images/$image',
+        width: width, height: height, fit: fit);
+  }
+}
